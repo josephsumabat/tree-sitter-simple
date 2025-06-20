d388baf38d5548469f805dd80f004ca6de26cc24
Joseph Sumabat <josephrsumabat@gmail.com>
2025-06-19 22:54:09 -0400

    Add unwrap maybe utility function

--- tree-sitter-ast/src/AST/Unwrap.hs
+++ tree-sitter-ast/src/AST/Unwrap.hs
@@ -6,3 +6,9 @@ import AST.Err
 
 class Unwrap n u | n -> u where
   unwrap :: n -> Err u
+
+unwrapMaybe :: Unwrap n u => n -> Maybe u
+unwrapMaybe v =
+  case unwrap v of
+    Left _e -> Nothing
+    Right v -> Just v
